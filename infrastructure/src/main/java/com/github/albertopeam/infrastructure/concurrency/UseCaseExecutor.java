cfd7a378807a40953e152939c97c5621ffce1117
Alberto Penas Amor <alberto__pa@hotmail.com>
2017-12-18 22:41:44 +0100

    updated javadoc

--- infrastructure/src/main/java/com/github/albertopeam/infrastructure/concurrency/UseCaseExecutor.java
+++ infrastructure/src/main/java/com/github/albertopeam/infrastructure/concurrency/UseCaseExecutor.java
@@ -9,11 +9,14 @@ import android.support.annotation.NonNull;
  */
 public interface UseCaseExecutor {
     /**
-     * Invoke this method to execute {@link UseCase} async, the {@link UseCase} is not going to be
-     * executed if the {@link LifecycleState} is not one of: CREATED || STARTED || RESUMED.
-     * @param args passed to {@link UseCase} for its exection
+     * Invoke this method to execute {@link UseCase} async, the {@link UseCase} is going to be
+     * executed if the {@link LifecycleState} is INITIALIZED and NOT DESTROYED.
+     * @param args passed to {@link UseCase} for its execution
      * @param useCase to execute in background thread
-     * @param successCallback to respond in main thread after the execution of the {@link UseCase}
+     * @param successCallback to respond a successful response in main thread after the execution of the {@link UseCase}
+     * @param exceptionCallback to respond that an {@link Exception } happens during the execution of the {@link UseCase}
+     * @param <Args> generic input
+     * @param <Response> generic response
      * @return true if added to the execution queue, false if already executing.
      */
     <Args, Response> boolean execute(final Args args,
